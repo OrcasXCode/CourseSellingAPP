501567eb065328b67d739c2e7ce2e908ce0d0e08
omsureja@gmail.com <118986497+OmPTEL@users.noreply.github.com>
2024-01-03 23:19:48 +0530

    This is auth for course-selling-app

---
+++ middleware/admin.js
@@ -0,0 +1,29 @@
+const {Admin} = require("../db")
+const jwt=require("jsonwebtoken")
+const {JWT_SECRET}=require("../config")
+// Middleware for handling auth
+async function adminMiddleware (req, res, next) {
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const token=req.headers.authorization;
+    const words=token.split(" ");
+    const jwtToken=words[1];
+    try{
+        const decodedValue=jwt.verify(jwtToken,JWT_SECRET);
+        if(decodedValue.username){
+            next();
+    }
+    else{
+        res.status(403).json({
+            msg:"Unautorized"
+        })
+    }
+    }
+    catch(e){
+        res.json({
+            msg:"Incorrect username or password"
+        })
+    }
+}
+
+module.exports = adminMiddleware;
